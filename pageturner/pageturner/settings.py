a255bb4fc0e7a8b988204acee70b071f953ef829
Anastasia Vakhrusheva <aaselezneva9@yandex.ru>
2024-01-26 14:25:29 +0400

    static files path

--- pageturner/pageturner/settings.py
+++ pageturner/pageturner/settings.py
@@ -147,7 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),
+                    'venv/lib/python3.11/site-packages/rest_framework/static']
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
