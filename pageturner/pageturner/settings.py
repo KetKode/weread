6cb098022c87032e87c90bb36766080eb8f7b264
Anastasia Vakhrusheva <aaselezneva9@yandex.ru>
2024-06-30 23:18:48 +0200

    added pagination

--- pageturner/pageturner/settings.py
+++ pageturner/pageturner/settings.py
@@ -179,6 +179,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 5
 }
 
 
